6755aae0242d0c279f4081f668457222c9fdab74
Evan Yang <71720197+Evany226@users.noreply.github.com>
2023-09-21 00:46:38 -0400

    changes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { AdminPage } from "./pages/admin-page";
+import { PageLoader } from "./components/page-loader";
 import { HomePage } from "./pages/home-page";
 import { NotFoundPage } from "./pages/not-found-page";
 import { ProfilePage } from "./pages/profile-page";
@@ -9,6 +11,16 @@ import { PublicPage } from "./pages/public-page";
 import { CallbackPage } from "./pages/callback-page";
 
 export const App = () => {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return (
+      <div className="page-layout">
+        <PageLoader />
+      </div>
+    );
+  }
+
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
